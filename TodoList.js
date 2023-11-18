3f645384298fb8887789fb52d4bcf6d3cbcc0a77
ashabhure <113898516+ashabhure@users.noreply.github.com>
2023-11-18 15:07:06 +0530

    Add files via upload
    TODo list Program using ReactJs

---
+++ TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+function TodoList() {
+const[activity,setActivity]=useState("")
+    return (
+      <>
+      <div className='container'>
+      <div className='header'>ToDo List</div>
+     <input type='text' placeholder='Add Activity' value={activity} onChange={(e)=> setActivity(e.target.value)}/>
+      </div>
+      </>
+    )
+  }
+export default TodoList
